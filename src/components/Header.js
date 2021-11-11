06e4ba7db30a6db1a626b3c2cbe46c9f219ba1a8
Cristian Montes <cmontesmujica@gmail.com>
2021-11-11 16:23:05 -0700

    added skeleton for home and cart

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <div>
+            <span>Contex API CART</span>
+            <ul>
+                <li>
+
+                </li>
+
+
+            </ul>
+        </div>
+    )
+}
+
+
+export default Header;
