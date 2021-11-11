a09241ddacfa2b93239736dd48271359263b8dde
Cristian Montes <cmontesmujica@gmail.com>
2021-11-11 16:27:14 -0700

    styles file added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
     return(
         <div>
             <span>Contex API CART</span>
-            <ul>
-                <li>
+            <ul className="nav">
 
+                <li className="prod">
+                    <Link to ="/">Home</Link>
                 </li>
-
+                <li className="prod1">
+                    <Link to ="/cart">Cart</Link>
+                </li>
 
             </ul>
         </div>
